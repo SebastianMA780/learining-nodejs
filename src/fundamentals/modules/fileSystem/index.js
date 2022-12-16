d496a5a94899444ad58532b72de5622202315390
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-12-15 20:40:52 -0500

    add read, write , delete a file

--- src/fundamentals/modules/fileSystem/index.js
+++ src/fundamentals/modules/fileSystem/index.js
@@ -5,10 +5,35 @@
 */
 const fs = require('fs');
 
+// Read a file from the system
 function readFile(route, callback) {
 	fs.readFile(route, (error, data) => {
-		console.log(data);
+		callback(data.toString());
 	})
 }
 
-readFile(__dirname + '/file.txt');
+readFile(__dirname + '/file.txt', console.log);
+
+//write a file
+function writeFile({ route, content, callback }) {
+	fs.writeFile(route, content, (error) => {
+		if (error) {
+			console.error('An error has ocurred', error);
+			} else {
+				callback('The file has been saved');
+			}
+		});
+}
+
+writeFile({
+	route: __dirname + '/file1.txt',
+	content: 'New file created',
+	callback: console.log,
+});
+	
+//delete a file
+function deleteFile(route, callback) {
+	fs.unlink(route, callback);
+}
+
+deleteFile(__dirname + '/file1.txt', console.log);
