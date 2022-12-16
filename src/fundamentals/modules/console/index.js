4f54630e584a7f4e6a1accca7c97f35dfc525cfe
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-12-15 20:49:21 -0500

    exploring console methods

---
+++ src/fundamentals/modules/console/index.js
@@ -0,0 +1,11 @@
+console.log(); //prints the message in the console.
+
+console.error('TEST ERROR'); // used to print errors in the console.
+
+console.table(); //prints a table in the console. array , objects.
+
+console.group('a thread of messages'); //prints a group of messages in the console. it has indentation.
+console.log('first message');
+console.log('second message');
+console.log('third message');
+console.groupEnd();
