1a1fb53a81b8ab4353fa1e0c536be1d601e415fb
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2023-01-17 20:13:36 -0500

    create node js swrver

---
+++ src/fundamentals/modules/http/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+	console.log('Request received');
+	console.log(req.url);
+
+	res.write('Hello World!');
+
+	res.end()
+}).listen(3000);
+
+console.log('Server started, listening on port 3000');
