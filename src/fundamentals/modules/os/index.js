c38224bff8901598e452f02c96057a94e7ec7f10
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2023-01-17 20:23:44 -0500

    os module info

---
+++ src/fundamentals/modules/os/index.js
@@ -0,0 +1,8 @@
+//os module allows us to access the operating system information.
+const os = require('os');
+
+console.log(os.arch()); //architecture of the operating system
+console.log(os.platform()); //platform of the operating system
+console.log(os.cpus()); //cpu information
+console.log(os.freemem()); //free memory
+console.log(os.homedir());  //home directory
